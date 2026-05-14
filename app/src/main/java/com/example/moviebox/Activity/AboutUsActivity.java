8fd8bd75a89ddace41dda5666fccffc0ff2efcc4
Dhananjay shinde <150016082+Dhananjaygit8010@users.noreply.github.com>
2026-05-14 22:25:07 +0530

    Update AboutUsActivity.java

--- app/src/main/java/com/example/moviebox/Activity/AboutUsActivity.java
+++ app/src/main/java/com/example/moviebox/Activity/AboutUsActivity.java
@@ -1,6 +1,6 @@
 package com.example.moviebox.Activity;
 import android.os.Bundle;
-//dd commit for dauly data manin
+//dd commit for daily graph 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
