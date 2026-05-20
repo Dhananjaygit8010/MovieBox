b7fbe02e786e9a27a38dcaa1292bb1eca2dbf954
Dhananjay shinde <150016082+Dhananjaygit8010@users.noreply.github.com>
2026-05-21 00:40:04 +0530

    Update AboutUsActivity.java

--- app/src/main/java/com/example/moviebox/Activity/AboutUsActivity.java
+++ app/src/main/java/com/example/moviebox/Activity/AboutUsActivity.java
@@ -1,6 +1,6 @@
 package com.example.moviebox.Activity;
 import android.os.Bundle;
-//dd commit and 
+//dd commit a
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
