5162da7ab55feb0b4f10c74217fdafb3f2fc663a
Dhananjay shinde <150016082+Dhananjaygit8010@users.noreply.github.com>
2026-06-07 15:37:11 +0530

    Fix comment typo in AboutUsActivity.java

--- app/src/main/java/com/example/moviebox/Activity/AboutUsActivity.java
+++ app/src/main/java/com/example/moviebox/Activity/AboutUsActivity.java
@@ -1,6 +1,6 @@
 package com.example.moviebox.Activity;
 import android.os.Bundle;
-// dd daily commit
+// dd daily co
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
